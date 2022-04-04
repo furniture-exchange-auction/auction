f50d2e8a50d1da0d1a425f7c30146d9a4a18f212
Eli Davis <eli.davis42@gmail.com>
2022-04-04 16:26:23 -0400

    front end components

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -145,7 +145,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function NavBar() {
   const [open, setOpen] = useState(false)
 
   return (
