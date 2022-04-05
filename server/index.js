a1ad79838056aae270aa1082cc3a10c6668a1f75
Kayliegh.Hill <kayliegh.hill@gmail.com>
2022-04-05 11:30:17 -0400

    add authentication route to server

--- server/index.js
+++ server/index.js
@@ -4,10 +4,20 @@ const express = require('express');
 
 const cors = require('cors');
 const path = require('path');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
 app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,12 +26,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../app/out')));
 
 // configure routes
+app.use('/api', authRouter);
+
+//test
 app.get('/api', (req, res, next) => {
   res.send('backend connected');
 });
 
+//respond with entry point to Next.js applciation
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../app/out/index.html'));
+});
+
+//Page not found catch-all
+app.use('/*', (req, res, next) => {
+  res.status(404).json('Page not found.');
 });
 
 // configire express global error handler
