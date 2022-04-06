d7784d4a1080dd2900e0560438b66566f082567a
Eli Davis <eli.davis42@gmail.com>
2022-04-06 11:04:55 -0400

    before merge

--- app/components/ProductList.jsx
+++ app/components/ProductList.jsx
@@ -42,7 +42,7 @@ export default function ProductList() {
 				<h2 className="sr-only">Products</h2>
 				<div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
 					{products.map((product) => (
-						<a key={product.id} href={product.href} className="group">
+						<a key={product.id} className="group">
 							<div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
 								<img
 									src={product.imageSrc}
@@ -56,12 +56,7 @@ export default function ProductList() {
 							  <div className="inline pl-2">
 							    <BidButton/>
 							  </div>
-							</div>
-							
-					
-							
-						
-							
+							</div>	
 						</a>
 					))}
 				</div>
