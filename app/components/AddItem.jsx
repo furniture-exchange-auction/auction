83489573a21203fcea6967d8b6483c421f16d6fa
Anthony <adavid16967@gmail.com>
2022-04-06 15:49:17 -0400

    added func to handle

--- app/components/AddItem.jsx
+++ app/components/AddItem.jsx
@@ -1,9 +1,36 @@
 import DropDown from "./DropDown";
-export default function AddItem() {
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, syncItems } from "../store/data-slice";
+import { itemActions } from "../store/data-slice";
 
+
+export default function AddItem() {
+  const dispatch = useDispatch()
+  const newItemName = useSelector((state) => state.data.setItemName)
+  const newPrice = undefined;
+  const newDescription = undefined;
+  const newUrl = undefined;
+  const alt_text = undefined;
+  const category = undefined;
+  const end_time = undefined;
+
+
+  const handleSubmit = (e) => {
+    return dispatch(addItem(body))
+      .then(() => dispatch(syncItems()))
+  }
+  const body = {
+    title: 'Couch',
+    opening_bid: 450,
+    description: 'large clean comfortable 3 seat sofa couch',
+    url: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+    alt_text: 'a couch',
+    category: 'Living Room',
+    end_time: '2022-04-17T08:00:00.000Z',
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div class="bg-indigo-50 min-h-screen md:px-20 pt-6">
         <div class=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
           <h1 class="text-center text-2xl font-bold text-gray-500 mb-10">
@@ -15,6 +42,7 @@ export default function AddItem() {
                 Title of Listing:
               </label>
               <input
+                onChange={(e) => dispatch(itemActions.setItemName(e.target.value))}
                 type="text"
                 placeholder="title"
                 id="title"
@@ -26,6 +54,7 @@ export default function AddItem() {
                 Description:
               </label>
               <textarea
+                onChange={(e) => body.description = e.target.value}
                 id="description"
                 cols="30"
                 rows="10"
